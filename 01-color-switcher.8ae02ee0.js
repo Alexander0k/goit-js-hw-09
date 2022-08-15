7de79243c39799f72c2590875103812a53d01c4c
Alexander0k <94919988+Alexander0k@users.noreply.github.com>
2022-08-15 21:37:17 +0000

    Deploying to gh-pages from @ Alexander0k/goit-js-hw-09@db9e098885256b09c7cfa9e4f26b0b47e56097ae 🚀

--- 01-color-switcher.8ae02ee0.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearTimeout(n),t.disabled=!1}));var n=null}();
-//# sourceMappingURL=01-color-switcher.8ae02ee0.js.map
